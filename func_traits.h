0c89e74ee5128f9d67fa9655ee1e697015e18527
dimitriGarrett <garrett.dimitri@gmail.com>
2023-03-01 13:26:59 -0500

    rename func_traits to function_traits

--- func_traits.h
+++ func_traits.h
@@ -6,12 +6,12 @@
 namespace function_traits
 {
 	template <typename Func>
-	struct func_traits : public func_traits<decltype(&std::decay_t<Func>::operator())>
+	struct function_traits : public function_traits<decltype(&std::decay_t<Func>::operator())>
 	{
 	};
 
 	template <typename R, typename... Args>
-	struct func_base
+	struct function_base
 	{
 		using return_type = R;
 
@@ -25,23 +25,23 @@ namespace function_traits
 	};
 
 	template <typename Func, typename... Args>
-	struct func_traits<Func(*)(Args...)> : public func_base<Func, Args...>
+	struct function_traits<Func(*)(Args...)> : public function_base<Func, Args...>
 	{
 	};
 
 	template <typename Func, typename... Args>
-	struct func_traits<Func(Args...)> : public func_base<Func, Args...>
+	struct function_traits<Func(Args...)> : public function_base<Func, Args...>
 	{
 	};
 
 	template <typename Class, typename Func, typename... Args>
-	struct func_traits<Func(Class::*)(Args...)> : public func_base<Func, Args...>
+	struct function_traits<Func(Class::*)(Args...)> : public function_base<Func, Args...>
 	{
 		using class_type = Class;
 	};
 
 	template <typename Class, typename Func, typename... Args>
-	struct func_traits<Func(Class::*)(Args...) const> : public func_base<Func, Args...>
+	struct function_traits<Func(Class::*)(Args...) const> : public function_base<Func, Args...>
 	{
 		using class_type = Class;
 	};
